c4a0c20cb142d2a414094002c6afc73900c0b389
PetrF20 <fabo@seznam.cz>
2023-04-03 15:00:19 +0200

    Přidání Inject DB context do StatusController.cs

--- InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsuranceCorp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InsuranceCorp.MVC.Controllers
 {
     public class StatusController : Controller
     {
+        private readonly InsCorpDbContext _context; //můžeme použít pro všechny metody
+
+        //Inject DB context
+        public StatusController(InsCorpDbContext context)
+        {
+            _context = context;
+
+        }
+
         public IActionResult Index()
         {
             return View();
