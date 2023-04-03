bc84d1b1c71166c7f5bf2f64896075edd3fa1503
PetrF20 <fabo@seznam.cz>
2023-04-03 11:39:28 +0200

    Přidaná stránka Hello

--- InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/HomeController.cs
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace InsuranceCorp.MVC.Controllers
             return View();
         }
 
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
